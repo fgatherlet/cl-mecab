e0ed2010ed5b3d838f0311f10ec76c36e728d0dd
fgatherlet <fgatherlet@gmail.com>
2019-04-25 21:46:10 +0900

    add api. modify to do with macports libmecab.

--- src/cl-mecab.lisp
+++ src/cl-mecab.lisp
@@ -14,54 +14,61 @@
            #:with-mecab
            #:with-mecab*
            #:parse
-           #:parse*))
+           #:parse*
+           ))
 (in-package :cl-mecab)
 
 
 (define-foreign-library libmecab
-    (:darwin "libmecab.dylib")
-    (:unix "libmecab.so")
-    (:windows "libmecab.dll"))
+  #+macports
+  (:darwin "/opt/local/lib/libmecab.dylib")
+  #-macports
+  (:darwin "libmecab.dylib")
+  (:unix "libmecab.so")
+  (:windows "libmecab.dll"))
 
 (use-foreign-library libmecab)
 
-(defcfun ("mecab_new2" %mecab_new2%) :pointer
+(defcfun ("mecab_new2" mecab_new2) :pointer
   (arg :string))
 
-(defcfun ("mecab_sparse_tostr" %mecab_sparse_tostr%) :string
+(defcfun ("mecab_sparse_tostr" mecab_sparse_tostr) :string
   (mecab :pointer) ; mecab_t
   (str :string))
 
-(defcfun ("mecab_destroy" %mecab_destroy%) :void
+(defcfun ("mecab_destroy" mecab_destroy) :void
   (mecab :pointer)) ; mecab_t
 
+(defcfun ("mecab_strerror" mecab_strerror) :string
+  (m :pointer) #|mecab_t|#)
+(defcfun ("mecab_version" mecab_version) :string)
 
 (defvar *mecab*)
 
 (defun make-mecab (&optional (option ""))
-  (%mecab_new2% option))
+  (mecab_new2 option))
 
 (defun load-tagger (&optional (option ""))
   (setf *mecab* (make-mecab option)))
 
 (defmacro with-mecab ((&optional (option "")) &body body)
   `(let ((*mecab* (load-time-value (make-mecab ,option))))
-     (%mecab_sparse_tostr% *mecab* "") ; avoiding MeCab bug
+     (mecab_sparse_tostr *mecab* "") ; avoiding MeCab bug
      nil
      ,@body))
 
 (defmacro with-mecab* ((&optional (option "")) &body body)
   `(let ((*mecab* (make-mecab ,option)))
-     (%mecab_sparse_tostr% *mecab* "") ; avoiding MeCab bug
+     (mecab_sparse_tostr *mecab* "") ; avoiding MeCab bug
      (unwind-protect
           (progn ,@body)
-       (%mecab_destroy% *mecab*))))
+       (mecab_destroy *mecab*))))
 
 (defun parse (text &optional (*mecab* *mecab*))
-  (%mecab_sparse_tostr% *mecab* text))
+  (mecab_sparse_tostr *mecab* text))
 
 (defun parse* (text &optional (*mecab* *mecab*))
-  (let* ((parse-result (%mecab_sparse_tostr% *mecab* text))
+  (let* ((parse-result (mecab_sparse_tostr *mecab* text))
          (lines (split-sequence #\Newline parse-result)))
 
     (loop
